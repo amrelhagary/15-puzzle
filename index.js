e0420f4720fafddbb76fef6dedb378d154d3a375
Amr Elhagary <amr.elhagary@gmail.com>
2018-06-23 02:04:58 -0500

    serving static content

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 80;
+
+app.use('/', express.static('dist'));
+
+app.listen(port, function() {
+    console.log(`app is running on ${port}`);
+});
